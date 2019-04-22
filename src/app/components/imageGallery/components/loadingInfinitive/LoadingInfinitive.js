b71663c329a886e00fab43f81d49ee624dd4c862
Bechev, Ivan <ivan.bechev@accenture.com>
2019-04-22 14:41:11 +0100

    Loading components and Dialog components refactored, minor style changes.

---
+++ src/app/components/imageGallery/components/loadingInfinitive/LoadingInfinitive.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useLoadingDots } from "../../../../utilities";
+
+const LoadingInfinitive = () => {
+  const dots = useLoadingDots();
+  return (
+    <div className="loading-infinitive">
+      <i className="fas fa-spinner fa-spin" />
+      <span className="title">Loading</span>
+      <span>
+        {dots.map((dot, i) => (
+          <span key={i}>.</span>
+        ))}
+        {dots}
+      </span>
+    </div>
+  );
+};
+
+export default LoadingInfinitive;
